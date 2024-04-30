245e988c3d1360d19a3f8eda6d624f777f1a6f7b
jelly <jelly.perwitasari@sirclo.com>
2024-04-30 16:22:09 +0700

    ADD TCR2-G.Validation of Registration Failure with Empty Mandatory Fields

--- resources/data/testdata.py
+++ resources/data/testdata.py
@@ -1,6 +1,11 @@
 #Account Detail
-EmailAddress            = 'demo@icube.us'
+EmailAddress            = 'jellyoms@yopmail.com'
+EmailAddressRegistered  = 'demo@icube.us'
 Password                = 'Password123'
+FirstName               = 'demo'
+LastName                = 'icube'
+PhoneNumber             = '085230233331'
+OtherPhoneNumber        = '123456789'
 
 #URL List
 URLWEB                  = 'https://swift-hyva.testingnow.me/'
---
+++ resources/locators/base_locator.py
@@ -0,0 +1,8 @@
+#Alert
+MessageSuccessAlert                     = "//span[@x-html='message.text']"
+RegisterAlertMessageFirtName            = "//ul[@id='accountcreate-1']"
+RegisterAlerMessageWeakPassword         = "//li[normalize-space()='Please enter at least 8 characters.']"
+RegisterAlertMessagePasswordNotMatch    = "//li[@data-msg-field='password_confirmation']"
+RegisterAlerrMessageInvalidRegister     = "//span[@x-html='message.text']"
+
+               
