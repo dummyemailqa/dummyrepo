67d4068c8d1e737cda110317416826d0c50ba75e
pangestuicube <pangestu.kurniawan@sirclo.com>
2024-05-07 15:50:11 +0700

    add TCL2-G

--- resources/locators/login_locator.py
+++ resources/locators/login_locator.py
@@ -2,3 +2,6 @@ FromLogin       = "//form[@id='customer-login-form']"
 LoginUsername   = "//input[@id='email']"
 LoginPassword   = "//input[@id='pass']"
 ButtonLogin     = "//button[@name='send']"
+
+#ErrorMessages
+AlertInvalidEmailFormat    = "//li[@data-msg-field='login[username]']"
