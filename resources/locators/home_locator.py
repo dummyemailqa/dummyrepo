b23ae1640b9f241009a4bf7cee4ff04daddd81d5
AntonIcube <anton.agape@sirclo.com>
2024-05-10 09:51:50 +0700

    TCPLP1.Customers able to Change product view as Grid on PLP

--- resources/locators/home_locator.py
+++ resources/locators/home_locator.py
@@ -3,6 +3,7 @@ HeaderLinkToLogin   = "//div[contains(@class,'header-icons-wrapper')]//button[@i
 ButtonSignIn        = "//a[@id='customer.header.sign.in.link']"
 ButtonSignOut       = "//a[@id='customer.header.sign.out.link']"
 UserLoggedInIcon    = "//div[contains(@class,'header-icons-wrapper')]//button[@id='customer-menu']//span[contains(@x-text,'customer.firstname')]"
+MenuWoman           = "//a[@title='Women']/span[@class='inline items-center']"
 
 #Search
 SearchBox           = "//input[@id='search']"
--- resources/locators/product_list_locator.py
+++ resources/locators/product_list_locator.py
@@ -1,1 +1,7 @@
-ProductItemCard         = "//main[@id='maincontent']//form[1]"
+ProductItemCard             = "//main[@id='maincontent']//form[1]"
+ProductsListViewIcon        = "//strong[@title='Grid']/preceding-sibling::button[@title='List']"
+ProductListViewContainer    = "//div[contains(@class,'mode-list')]"
+ProductsGridViewIcon        = "//strong[@title='List']/following-sibling::button[@title='Grid']"
+ProductGridViewContainer    = "//div[contains(@class,'mode-grid')]"
+
+
