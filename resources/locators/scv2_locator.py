976182bf7e0111212ff797a549e186039b6319a9
pangestuicube <pangestu.kurniawan@sirclo.com>
2024-06-07 07:55:02 +0700

    add locator

--- resources/locators/scv2_locator.py
+++ resources/locators/scv2_locator.py
@@ -2,3 +2,8 @@
 ButtonCheckoutogInSCV       = "//button[@id='checkout-login-continueButton']"
 InputPhoneNumberLogin       = "//input[@id='checkout-loginPhoneNumber-textField']"
 AlertMessageLoginFaild      = "//p[contains(@class,'textField-errorLabel')]"
+InputOTPLogin               = "//input[@id='checkout-loginOtp-textField']"
+FirstOtpVerificationMethod  = "//div[@role='dialog']//ul//div[@role='button']"
+
+# checkoutpage
+CheckoutPage                = "//div[contains(@class,'checkout-contact-title')]"
